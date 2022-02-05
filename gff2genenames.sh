c9b1303c6fc175868f2537ec8b9059ddef135e4c
kogamanya <isaackingsley85@yahoo.com>
2022-02-05 15:01:42 +1100

    Detailed SV analysis on study SRP199233

--- gff2genenames.sh
+++ gff2genenames.sh
@@ -2,3 +2,9 @@
 awk '$3=="gene"' Mus_musculus.GRCm38.98.gff3 \
 | cut -f9 | cut -d ';' -f-2 | cut -d ':' -f2- \
 | sed 's/;Name=/\t/'  > Mus_musculus.GRCm38.98.gff3.genenames.tsv
+
+
+
+awk '$3=="gene"' Mus_musculus.GRCm39.104.gff3.gz \
+| cut -f9 | cut -d ';' -f-2 | cut -d ':' -f2- \
+| sed 's/;Name=/\t/'  > Mus_musculus.GRCm39.104.gff3.genenames.tsv
