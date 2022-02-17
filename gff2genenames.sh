80d351f70b74a7c80c4100ffe6be5f6cb93d31da
kogamanya <isaackingsley85@yahoo.com>
2022-02-17 17:22:50 +1100

    unzipped the gff file

--- gff2genenames.sh
+++ gff2genenames.sh
@@ -5,6 +5,6 @@ awk '$3=="gene"' Mus_musculus.GRCm38.98.gff3 \
 
 
 
-awk '$3=="gene"' Mus_musculus.GRCm39.104.gff3.gz \
+awk '$3=="gene"' Mus_musculus.GRCm39.104.gff3 \
 | cut -f9 | cut -d ';' -f-2 | cut -d ':' -f2- \
 | sed 's/;Name=/\t/'  > Mus_musculus.GRCm39.104.gff3.genenames.tsv
